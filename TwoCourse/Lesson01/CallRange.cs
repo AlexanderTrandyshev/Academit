030a8d57a7db82bcfa1989a58be45c8edfbd6051
Alexander <AlexanderTrandyshev@yandex.ru>
2018-07-09 08:35:59 +0700

    Доработка пересечения

--- TwoCourse/Lesson01/CallRange.cs
+++ TwoCourse/Lesson01/CallRange.cs
@@ -9,9 +9,24 @@ namespace Range
         public static void Main(string[] args)
         {
             Range range1 = new Range(1, 10);
-            Console.WriteLine("Длинна отрезка = {0}", range1.GetLengthInterval);
-            Console.WriteLine("Точка лежит на отрезке: {0}", range1.IsInside(5));
-            Console.WriteLine("Пересечение интервалов: " + range1.GetIntersectionIntervals(5, 10));
+            Range range2 = new Range(5, 10);
+
+            Console.WriteLine($"Длинна отрезка = {range1.GetLengthInterval}");
+            Console.WriteLine($"Точка лежит на отрезке: {range1.IsInside(5)}");
+            // Как получить от "GetIntersectionIntervals" целое значение?
+            Console.WriteLine($"Пересечение интервалов: {range1.GetIntersectionIntervals(range2)}");
+
+            /*GetIntersectionIntervals
+             *Сделать не через структуру? - null не передает (не ref)
+             * Сделать через объект? - как можно обратиться к полям передоваемого класса? (присвоить полям и вернуть?(-_-))
+             * Сделать через кортежи? - null не передает (не ref)
+             * Сделать через массив? - не печатает сами точки а объект. (можно создавать ф-ию для печати? или сделать стринг(не хочется ведь это точки)?)
+            
+            
+            пересечение - не статические методы
+             объединение - массив объектов
+             
+             */
         }
     }
 }
--- TwoCourse/Lesson01/Range.cs
+++ TwoCourse/Lesson01/Range.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+//спросить про правильное написание
 namespace Range
 {
     class Range
@@ -17,6 +17,18 @@ namespace Range
             set;
         }
 
+        public static double NewFrom
+        {
+            get;
+            set;
+        }
+
+        public static double NewTo
+        {
+            get;
+            set;
+        }
+
         public double GetLengthInterval
         {
             get { return To - From; }
@@ -28,45 +40,130 @@ namespace Range
             To = to;
         }
 
-        public double[] GetIntersectionIntervals(double start, double end)
+        // Пересечения 
+        /*
+        public Interval GetIntersectionIntervals(double start, double end)
         {
-            //Условие, что интервалы пересекаются
-            if (From <= end && To >= start)
+            // if(From <= end && To >= end)   !(A1 > B2 || A2 < B1) - условие пересечения?
+
+            double[] newInterval = new double[5];
+            double i = From;
+            int j = 0;
+            double e = 1e11;
+
+            while (i < To || Math.Abs(To - i) == e)
             {
-                double[] newInterval = new double[(int)Math.Round(GetLengthInterval)];
-                double[] startEndNewInterval = new double[2];
-
-                for (int i = 0, j = 0; i <= To; ++i)
+                if ((i > start && i < end) || Math.Abs(i - end) == 0 || Math.Abs(i - start) == 0)
                 {
-                    // условие, что i пренадлежит интервалу №2
-                    // TODO не правильно сравнивает вещественное число!?
-                    if (i >= start && i <= end)
-                    {
-                /* Сейчас массив может быть не полностью заполнен или переполнен и нужно изменять его размерность*/
-                        newInterval[j] = i;
-                        ++j;
+                    newInterval[j] = i;
 
-                        // TODO написать код для расширения массива newInterval
+                    if (j >= newInterval.Length - 1)
+                    {
+                        double[] oldnewInterval = newInterval;
+                        newInterval = new double[oldnewInterval.Length * 2];
+
+                        oldnewInterval.CopyTo(newInterval, 0);
                     }
 
+                    ++j;
                 }
-                // TODO написать код для размера массива newInterval сужения в случае его полного заполнения
-                // беруться 1 и последний элемент массива (учесть что это не одна и таже точка)
-                startEndNewInterval[0] = newInterval[0]; 
-                startEndNewInterval[1] = newInterval[newInterval.Length - 1];
-                
-                // массив передается с двумя значениями или одним (если пересечение в одной точке)
-                return startEndNewInterval;
+
+                ++i;
             }
-            else
+
+            if (j == 0)
+            {
+                // как в struct передать null? 
+                return new Interval(null);
+            }
+            // TODO беруться 1 и последний элемент массива (учесть что это не одна и таже точка ?)
+            double[] usedNewInterval = new double[j];
+
+            for (int k = 0; k < j; ++k)
+            {
+                usedNewInterval[k] = newInterval[k];
+            }
+
+            double[] arrayTop = { usedNewInterval[0], usedNewInterval[usedNewInterval.Length - 1] };
+            return new Interval(usedNewInterval[0], usedNewInterval[usedNewInterval.Length - 1]);
+        }
+        */
+
+        // попытка через Object
+
+        public Object GetIntersectionIntervals(Range interval)
+        {
+            // if(From <= end && To >= end)   !(A1 > B2 || A2 < B1) - условие пересечения?
+            double[] newInterval = new double[5];
+            double i = From;
+            int j = 0;
+            //double e = 1e-11;
+
+            while (i <= To)
+            {
+                if (i >= interval.From && i <= interval.To)
+                {
+                    newInterval[j] = i;
+
+                    if (j >= newInterval.Length - 1)
+                    {
+                        double[] oldnewInterval = newInterval;
+                        newInterval = new double[oldnewInterval.Length * 2];
+
+                        oldnewInterval.CopyTo(newInterval, 0);
+                    }
+
+                    ++j;
+                }
+
+                ++i;
+            }
+
+            if (j == 0)
             {
                 return null;
             }
+
+            double[] usedNewInterval = new double[j];
+
+            for (int k = 0; k < j; ++k)
+            {
+                usedNewInterval[k] = newInterval[k];
+            }
+
+            return new Range(NewFrom = usedNewInterval[0], NewTo = usedNewInterval[usedNewInterval.Length - 1]);
         }
+
+        // Объединение
+        /*
+         * public double[] GetJoinIntervals(double start, double end)
+        {
+
+        }
+        */
 
         public bool IsInside(double point)
         {
             return point >= From && point <= To;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"{NewFrom}, {NewTo}";
+        }
+
+        public struct Interval
+        {
+            public readonly double start;
+            public readonly double end;
+
+            public Interval(double _start, double _end)
+            {
+                start = _start;
+                end = _end;
+            }
         }
     }
 }
