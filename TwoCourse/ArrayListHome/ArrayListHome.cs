e0bb9478ed58a928944bd143b53f820eb12434e1
Alexander Trandyshev <trandyshev@bacup.it>
2018-07-23 13:22:55 +0700

    Add last change ArrayListHome

--- TwoCourse/ArrayListHome/ArrayListHome.cs
+++ TwoCourse/ArrayListHome/ArrayListHome.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,43 @@ namespace ArrayListHome
     {
         static void Main(string[] args)
         {
-            //TODO добавить using читение из файла четных\нечетных чисел с try-catch проверкой на null в файле
+            //TODO добавить путь, где лежит файл
+            const string path = "ПУТЬ ДЛЯ ФАЙЛА ГДЕ ВСЕ ЧИСЛА";
 
-            List<int> listNumber = new List<int>();
-            
-            //TODO добавить цикл для listNumber.Add нового значения из файла в список с if(без четных чисел).
-            //TODO В списке удалить все нечетных чисела.
-            
+            using (StreamReader sr = new StreamReader(path))
+            {
+                if (sr.ReadToEnd() != null)
+                {
+                    throw new Exception("Пустой файл");
+                }
+                else
+                {
+                    string fileCopy =sr.ReadToEnd();
+
+                    List<int> listNumber = new List<int>();
+
+                    for (int i = 0; i < fileCopy.Length; ++i)
+                    {
+                        //TODO добавить реализацию добавления данных в список
+
+                        /*
+                         * Разбить строку на отдельные числа
+                          Конверктировать строку в число
+                          Добавить все числа в список
+                        */
+                    }
+
+                    //TODO удалить из списка (отдельным циклом) все  четные числа
+                }
+
+                //TODO добавить реализацию сравнения каждого числа и все числа добавить в новый список без повторения.
+                /* Двойной\Одинарный цикл.
+                 * Сравнение одного числа с другими в списке.
+                 * Длина первого цикла n-1, а второй до конца списка.
+                 * Опирация сравнения должна быть пример(есть число в 1 списке проверяет есть ли такое число во втором, если нет то добавит число иначе берет следующее)
+                 * 
+                 */
+            }
         }
     }
 }
