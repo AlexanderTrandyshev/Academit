5c2f1e0d2143ac99de8a460add23bc36b0da4e87
Alexander Trandyshev <trandyshev@bacup.it>
2018-08-14 11:47:14 +0700

    В задачу Вектор добавил реализацию п.4 (a, b, c, d, e, f)

--- TwoCourse/Vector/MethodsVector.cs
+++ TwoCourse/Vector/MethodsVector.cs
@@ -19,7 +19,10 @@ namespace Vector
             }
             else
             {
-                component = null;
+                for (int i = 0; i < component.Length; ++i)
+                {
+                    component[i] = 0;
+                }
                 this.size = size;
             }
         }
@@ -27,44 +30,46 @@ namespace Vector
         public MethodsVector(MethodsVector vector)
         {
             size = vector.size;
-            this.component = vector.component;
+            Array.Copy(component ,vector.component, vector.component.Length);
+        }
+         
+        public MethodsVector(double[] valuesArray)
+        {
+            // TODO Нужно не создавать копию, а сразу дополнить значениями из массива?
+            Array.Copy(component, valuesArray, valuesArray.Length);
         }
 
-        public MethodsVector(double[] vector)
+        public MethodsVector(double sizeArray, double[] valuesArray)
         {
-            for (int i = 0; i < vector.Length; ++i)
+            for (int i = 0; i < valuesArray.Length; ++i)
             {
-                this.component[i] = vector[i];
-            }
-        }
-
-        public MethodsVector(double sizeArray, double[] arrayComponetsVector)
-        {
-            for (int i = 0; i < arrayComponetsVector.Length; ++i)
-            {
-                if (arrayComponetsVector.Length < sizeArray && i > sizeArray)
+                if (valuesArray.Length < sizeArray && i > sizeArray)
                 {
                     component[i] = 0;
                 }
                 else
                 {
-                    component[i] = arrayComponetsVector[i];
+                    component[i] = valuesArray[i];
                 }
             }
         }
 
-        public double GetSize(MethodsVector vector)
-        {
-            return size;
-        }
-
         public override string ToString()
         {
-            return string.Join(",", component);
+            string lineComponent = string.Join(",", component);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{ ")
+              .Append(lineComponent)
+              .Append(" }");
+
+            string result = sb.ToString();
+
+            return result;
         }
 
-        //TODO нет полной реализации
-        public double AdditionVectors(MethodsVector vector)
+        // TODO Size - что происходит с этим значением? Нет конструктора создания обычного объекта.
+        public MethodsVector AdditionVectors(MethodsVector vector)
         {
             List<double> newComponent = new List<double>();
 
@@ -82,8 +87,59 @@ namespace Vector
                 }
             }
 
+            return new MethodsVector(newComponent.ToArray());
+        }
+        // TODO Size - что происходит с этим значением? Нет конструктора создания обычного объекта.
+        public MethodsVector SubtractionVectors(MethodsVector vector)
+        {
+            List<double> newComponent = new List<double>();
 
-            return new MethodsVector();
+            for (int i = 0; i < vector.component.Length; ++i)
+            {
+
+                //TODO компоненты меньшей дленны должны заполняться "0" 
+                if (vector.component.Length < component.Length)
+                {
+                    newComponent.Add(component[i] - vector.component[i]);
+                }
+                else
+                {
+                    newComponent.Add(component[i]);
+                }
+            }
+
+            return new MethodsVector(newComponent.ToArray());
+        }
+        // Плохой конструктор - зануляет все компоненты вектора.
+        public MethodsVector MultiplicationByScalar(double number)
+        {
+            double newSize = size * number;
+            return new MethodsVector(newSize);
+        }
+        
+        public MethodsVector TurnVector(MethodsVector vector)
+        {
+            for (int i = 0; i < vector.component.Length; ++i)
+            {
+                vector.component[i] *= -1;
+            }
+
+            return new MethodsVector(vector.size, vector.component);
+        }
+
+        public double GetSize(MethodsVector vector)
+        {
+            return vector.size;
+        }
+
+        public void SetComponentVector(double value, int index)
+        {
+            component.SetValue(value, index);
+        }
+
+        public double GetComponentVector(int index)
+        {
+            return Convert.ToDouble(component.GetValue(index));
         }
     }
 }
