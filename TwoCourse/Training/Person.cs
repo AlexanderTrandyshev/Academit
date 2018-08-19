b46f9fbac11d2cafc27e90cf109cf4516224bd17
Alexander <AlexanderTrandyshev@yandex.ru>
2018-08-20 01:24:42 +0700

    добавид проект для Training и использовал лямбда выражения

---
+++ TwoCourse/Training/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    class Person
+    {
+        public string Name { get; }
+        public int Age { get; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", Name);
+        }
+    }
+
+    class Call
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<Person> { new Person("Misha", 10), new Person("Ura", 20), new Person("Misha", 20) };
+            var uniqueName = list.Select(n => n.Name).Distinct().ToArray();
+            //var uniqueName = list.Where(p => p.Age < 18 ).Average(p => p.Age).Distinct(n => n.Name).ToArray().ToString();
+
+            Console.WriteLine($"{uniqueName}");
+        }
+    }
+}
